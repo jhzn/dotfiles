3650ffab5c7d7fe4c361ba0adbae5310f239ba82
Johan Håkansson <jhakanzon@gmail.com>
2019-07-19 15:59:37 +0200

    Init repo

---
+++ .config/dotfiles/bash_ops.sh
@@ -0,0 +1,7 @@
+stty -ixon # Disable ctrl-s and ctrl-q in terminals.
+shopt -s autocd #Allows you to cd into directory merely by typing the directory name.
+HISTSIZE= HISTFILESIZE= # Infinite history.
+
+source ~/bin/scripts/helpers.sh
+
+
---
+++ .config/dotfiles/ps1.sh
@@ -0,0 +1,103 @@
+COLOR_RED="\033[0;31m"
+COLOR_YELLOW="\033[0;33m"
+COLOR_GREEN="\033[0;32m"
+COLOR_OCHRE="\033[38;5;95m"
+COLOR_BLUE="\033[0;34m"
+COLOR_WHITE="\033[0;37m"
+COLOR_RESET="\033[0m"
+
+function git_color {
+  local git_status="$(git status 2> /dev/null)"
+
+  if [[ ! $git_status =~ "working directory clean" ]]; then
+    echo -e $COLOR_RED
+  elif [[ $git_status =~ "Your branch is ahead of" ]]; then
+    echo -e $COLOR_YELLOW
+  elif [[ $git_status =~ "nothing to commit" ]]; then
+    echo -e $COLOR_GREEN
+  else
+    echo -e $COLOR_OCHRE
+  fi
+}
+
+
+function git_branch {
+  local git_status="$(git status 2> /dev/null)"
+  local on_branch="On branch ([^${IFS}]*)"
+  local on_commit="HEAD detached at ([^${IFS}]*)"
+
+  if [[ $git_status =~ $on_branch ]]; then
+    local branch=${BASH_REMATCH[1]}
+    echo "($branch)"
+  elif [[ $git_status =~ $on_commit ]]; then
+    local commit=${BASH_REMATCH[1]}
+    echo "($commit)"
+  fi
+}
+
+
+
+
+## Colors?  Used for the prompt.
+#Regular text color
+BLACK='\[\e[0;30m\]'
+#Bold text color
+BBLACK='\[\e[1;30m\]'
+#background color
+BGBLACK='\[\e[40m\]'
+RED='\[\e[0;31m\]'
+BRED='\[\e[1;31m\]'
+BGRED='\[\e[41m\]'
+GREEN='\[\e[0;32m\]'
+BGREEN='\[\e[1;32m\]'
+BGGREEN='\[\e[1;32m\]'
+YELLOW='\[\e[0;33m\]'
+BYELLOW='\[\e[1;33m\]'
+BGYELLOW='\[\e[1;33m\]'
+BLUE='\[\e[0;34m\]'
+BBLUE='\[\e[1;34m\]'
+BGBLUE='\[\e[1;34m\]'
+MAGENTA='\[\e[0;35m\]'
+BMAGENTA='\[\e[1;35m\]'
+BGMAGENTA='\[\e[1;35m\]'
+CYAN='\[\e[0;36m\]'
+BCYAN='\[\e[1;36m\]'
+BGCYAN='\[\e[1;36m\]'
+WHITE='\[\e[0;37m\]'
+BWHITE='\[\e[1;37m\]'
+BGWHITE='\[\e[1;37m\]'
+
+PROMPT_COMMAND=smile_prompt
+
+function smile_prompt
+{
+	if [ "$?" -eq "0" ]
+	then
+		#smiley
+		SC="${GREEN}:)"
+		#SC="🍀"
+	else
+		#frowney
+		SC="${RED}:("
+		#SC="🔥"
+	fi
+	if [ $UID -eq 0 ]
+	then
+	#root user color
+	UC="${RED}"
+	else
+	#normal user color
+	UC="${CYAN}"
+	fi
+	#hostname color
+	HC="${BYELLOW}"
+	#regular color
+	RC="${BWHITE}"
+	#default color
+	DF='\[\e[0m\]'
+
+	PS1="[${UC}\u@${RC}\W${DF}]"
+	PS1+="\[\$(git_color)\]"        # colors git status
+	PS1+="\$(git_branch)"           # prints current branch
+	PS1+=" ${SC}${DF} "
+}
