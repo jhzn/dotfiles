e706696beeb40ed06f08088cf6a9b38bcefb72c2
Johan Håkansson <jhakanzon@gmail.com>
2019-09-15 19:55:53 +0200

    Clean up on_file_change.sh script

--- bin/scripts/on_file_change.sh
+++ bin/scripts/on_file_change.sh
@@ -8,14 +8,17 @@
 [ -z $1 ] && echo "Missing first arg which is the script to run whenever a file has been changed" && exit 1
 
 #This is the script which is be run whenever a file has been changed
-SCRIPT_PARAM=$1
+SCRIPT_TO_RUN=$1
+[ ! -x "$SCRIPT_TO_RUN" ] && echo "'$SCRIPT_TO_RUN' file is not executable. Try chmod +x" && exit 1
+
+#Default input to the $SCRIPT_TO_RUN
+SCRIPT_TO_RUN_ARGS="${@:2}"
 
 inotifywait -e close_write,moved_to,create -m -r . |
 while read -r directory events filename; do
-	if [[ -z ${@:2} ]]; then
-		$SCRIPT_PARAM "$filename"
-	elif [[ "${@:2}" =~ "$filename" ]]; then
-		echo "BANAN"
-		$SCRIPT_PARAM "$filename"
+	if [[ -z "$SCRIPT_TO_RUN_ARGS" ]]; then
+		bash $SCRIPT_TO_RUN "$filename"
+	elif [[ "$SCRIPT_TO_RUN_ARGS" =~ "$filename" ]]; then
+		bash $SCRIPT_TO_RUN "$filename"
 	fi
 done
