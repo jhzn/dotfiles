3812195355d8d6cc2b77db12803d4655e6599e09
Johan Håkansson <jhakanzon@gmail.com>
2019-09-22 20:58:57 +0200

    Cleaned up script

--- bin/scripts/on_file_change.sh
+++ bin/scripts/on_file_change.sh
@@ -2,8 +2,7 @@
 
 #if more arguments are passed than a script path then those arguments are then taken as files which the subsequent script is only to be applied to
 #example ./this_script.sh some_random_echo_script.sh $filename
-#$filename refers to the local variable of the file that has been changed
-#This would mean that some_random_echo_script.sh would only execute if the file that was changed was $filename
+#this would mean that some_random_echo_script.sh would only execute if the file that was changed was $filename
 
 [ -z $1 ] && echo "Missing first arg which is the script to run whenever a file has been changed" && exit 1
 
