b673a694b04e012e2efc6acdbbdce0ceb10f6e9d
Johan Håkansson <jhakanzon@gmail.com>
2019-10-02 09:05:42 +0200

    Updated vscode plugins

--- bin/scripts/helpers.sh
+++ bin/scripts/helpers.sh
@@ -10,14 +10,3 @@ function cat_all_and_search { find . -type f | xargs cat | grep -I -n -H $1; }
 function base64_encoded_to_png { echo $1 | sed -e 's#data:image/png;base64,##' | base64 --decode > out.png && echo "Created file named out.png"; }
 
 function json_pretty { xclip -o | jq; }
-
-function line_count_files_with_extension_in_cwd { 
-	if [ -n "$1" ]
-	then 
-		find . -name '*.'"$1" | xargs wc -l | sort -nr
-	else
-		echo 'Missing file extension as 1st argument. Example: txt, md, sh'
-	fi;
-} 
-
-function bananare { echo $1; }
