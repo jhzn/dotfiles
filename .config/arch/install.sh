7c48123d524f3cf290eeeed5fc95bb601d65bc97
Johan Håkansson <jhakanzon@gmail.com>
2020-06-23 20:20:46 +0200

    Added --needed flag to pacman commands so that the install script can be run easily/frequently to achieve idempotence of installed packages

--- .config/arch/install.sh
+++ .config/arch/install.sh
@@ -4,19 +4,19 @@
 
 
 #xorg / GUI
-pacman -S xorg bspwm sxhkd dmenu rofi dunst xscreensaver \
+pacman -S --needed xorg bspwm sxhkd dmenu rofi dunst xscreensaver \
 	xdg-utils lxsession xorg-setxkbmap xorg-xinit xf86-input-libinput \
 	gnome-keyring libsecret \
 	alacritty firefox network-manager-applet  \
 	xclip redshift picom tmux transmission-gtk unclutter feh
 
 #audio
-pacman -S pulseaudio pulsemixer pavucontrol playerctl
+pacman -S --needed pulseaudio pulsemixer pavucontrol playerctl
 #bluetooth
-pacman -S bluez bluez-utils pulseaudio-bluetooth
+pacman -S --needed bluez bluez-utils pulseaudio-bluetooth
 systemctl enable bluetooth && systemctl start bluetooth
 #dev machine
-pacman -S python2 python3 go nodejs yarn rustup gnu-netcat openssh docker docker-compose
+pacman -S --needed python2 python3 go nodejs yarn rustup gnu-netcat openssh docker docker-compose
 #Install rust toolchain
 rustup install stable
 rustup default stable
@@ -24,19 +24,19 @@ rustup default stable
 
 #setup vim
 #check /README.md
-pacman -S python-pynvim
+pacman -S --needed python-pynvim
 #theme/fonts
-pacman -S noto-fonts ttf-joypixels adapta-gtk-theme papirus-icon-theme lxappearance
+pacman -S --needed noto-fonts ttf-joypixels adapta-gtk-theme papirus-icon-theme lxappearance
 #intel
-pacman -S xf86-video-intel intel-ucode
+pacman -S --needed xf86-video-intel intel-ucode
 #utils
-pacman -S sxiv gimp zathura zathura-pdf-poppler fzf ripgrep jq
+pacman -S --needed sxiv gimp zathura zathura-pdf-poppler fzf ripgrep jq
 #files
-pacman -S unzip zip nemo pcmanfm
+pacman -S --needed unzip zip nemo pcmanfm
 #disks
-pacman -S gnome-disk-utility gparted udiskie ncdu
+pacman -S --needed gnome-disk-utility gparted udiskie ncdu
 #laptop
-pacman -S powertop tlp tlp-rdw
+pacman -S --needed powertop tlp tlp-rdw
 systemctl enable tlp && systemctl start tlp
 
 
