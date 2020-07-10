780273241b337aa56a53cfbd8a4c033523a1c2d9
Johan Håkansson <jhakanzon@gmail.com>
2020-07-10 14:17:51 +0200

    Improved arch install script. Replaced xscreensaver with xss-lock+slock for minimalism's sake

--- .config/arch/aur.sh
+++ .config/arch/aur.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
-#AUR, need Golang to build
-#https://github.com/Jguer/yay
-#git clone https://aur.archlinux.org/yay.git
-#cd yay
-#makepkg -si
+if [ ! $(which yay) ]; then
+	echo "AUR helper, yay not found, installing.."
+	#Need golang to build
+	#https://github.com/Jguer/yay
+	git clone https://aur.archlinux.org/yay.git
+	cd yay
+	makepkg -si
+	echo "AUR helper, yay is now installed!"
+fi
 
 yay -S --needed polybar ytop tealdeer-git lf vscodium-bin nerd-fonts-jetbrains-mono zeal rofi-greenclip passff-host
