e9e0750ed46e9f2d0f6420cb78fa98a5bc16033a
Johan Håkansson <jhakanzon@gmail.com>
2020-08-03 00:10:07 +0200

    Fixed dotfiles bootstrap script

--- .config/dotfiles/git.sh
+++ .config/dotfiles/git.sh
@@ -12,5 +12,6 @@ git config --global alias.wip "! git add . && git commit -m 'WIP' && git push"
 git config --global core.editor "nvim"
 git config --global core.eol "lf"
 git config --global core.autocrlf "input"
+git config --global pull.ff only
 
 echo "Updated global git config"
