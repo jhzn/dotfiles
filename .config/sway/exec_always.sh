bdcc99c23c9b27de811f13ec20c2c742951c8751
Johan Håkansson <jhakanzon@gmail.com>
2021-06-30 12:14:13 +0200

    Sway alttab script now works across multiple workspaces on the same monitor

--- .config/sway/exec_always.sh
+++ .config/sway/exec_always.sh
@@ -4,4 +4,11 @@
 
 #set previously used monitor layout
 #lay.sh populates this file
+echo "Running layout recreation"
 [ -f "$MONITOR_LAYOUT_FILE" ] && sh $(cat "$MONITOR_LAYOUT_FILE")
+
+if [ "$?" -eq 0 ]; then
+	echo "Layout recreation succeded"
+else
+	echo "Layout recreation failed"
+fi
