5fcbc9c6805245ff64fb1e49befa7cb6c57fa1b7
Johan Håkansson <jhakanzon@gmail.com>
2022-01-01 17:15:36 +0100

    Implemented ddcutil as backend in brightness.sh script to increase/decrease brightness of connected monitors

--- bin/scripts/brightness.sh
+++ bin/scripts/brightness.sh
@@ -1,16 +1,52 @@
-#!/bin/sh
+#!/bin/bash -x
 
-#just a minor wrapper around brightnessctl to reach 1% when we hit 5% and want to decrease brightness further down
+source ~/.config/dotfiles/bash_strict_mode.sh
 
-#TODO fix inefficiency
-current_level=$(brightnessctl | grep Current | awk '{print $4}' | sed -e 's/(//' -e 's/)//' -e 's/%//')
-increment=5
+backend="laptop"
+if [[ $(command -v ddcutil detect) ]]; then
+	backend="ddcutil"
+fi
 
-case "$1" in
-	up)
-		[ "$current_level" -eq 1 ] && increment=4
-		brightnessctl set +"$increment"%;;
-	down )
-		[ "$current_level" -eq 5 ] && increment=4
-		brightnessctl set "$increment"%-;;
-esac
+if [[ "$backend" == "laptop" ]]; then
+	#just a minor wrapper around brightnessctl to reach 1% when we hit 5% and want to decrease brightness further down
+
+	#TODO fix inefficiency
+	current_level=$(brightnessctl | grep Current | awk '{print $4}' | sed -e 's/(//' -e 's/)//' -e 's/%//')
+	increment=5
+
+	case "$1" in
+		up)
+			[ "$current_level" -eq 1 ] && increment=4
+			brightnessctl set +"$increment"%;;
+		down )
+			[ "$current_level" -eq 5 ] && increment=4
+			brightnessctl set "$increment"%-;;
+	esac
+fi
+
+if [[ "$backend" == "ddcutil" ]]; then
+	displays=$(ddcutil detect | grep Display | awk '{print $2}')
+
+	# just get for 1 display, not pretty but simple
+	current_level=$(ddcutil --display=1 -t getvcp 10 | awk '{print $4}')
+	increment=10
+	case "$1" in
+		up)
+			# brightnessctl set +"$increment"%;;
+			new_level=$(( $current_level + $increment));;
+		down )
+			new_level=$(( $current_level - $increment));;
+	esac
+
+	# Protection against hitting above/below boundries
+	if (( "$new_level" >= 100 )); then
+		new_level=100
+	fi
+	if (( "$new_level" <= 10 )); then
+		new_level=10
+	fi
+
+	for d in $displays;do
+		ddcutil --display="$d" setvcp 10 "$new_level"
+	done
+fi
