e63c43d9b2c73e2ea846866cfc58dac11d98c2f4
Johan Håkansson <jhakanzon@gmail.com>
2022-10-21 08:01:04 +0200

    Added Github actions and added unittest to be run there

---
+++ .config/dotfiles/unit_test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -euo pipefail
+
+echo "Running Unittests for the dotfiles"
+
+python ~/.config/sway/monitor_configer/generate_test.py
+
+# Make sure these parse as valid JSON
+jq '.' .config/waybar/aux_conf_template .config/waybar/primary_conf_template > /dev/null
+
+echo "All is OK! 🎉🎉"
