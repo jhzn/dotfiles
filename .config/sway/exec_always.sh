1db1da934437c3cf376618e3dc752906caa85046
Johan Håkansson <jhakanzon@gmail.com>
2023-01-18 07:35:40 +0100

    sway layout script changes

--- .config/sway/exec_always.sh
+++ .config/sway/exec_always.sh
@@ -1,14 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 
 source ~/.config/dotfiles/bash_strict_mode.sh
 
 ~/.config/sway/wallpaper.sh &
 
-#set previously used monitor layout
-#lay.sh populates this file
+# Set previously used monitor layout
+# lay populates this file
 echo "Running layout recreation"
 sleep 3 #necessary to get consistent results
-[ -f "$MONITOR_LAYOUT_FILE" ] && sh $(cat "$MONITOR_LAYOUT_FILE")
+if [[ ! -f "$MONITOR_LAYOUT_FILE" ]]; then
+	notify-send "Monitor Layout" "$MONITOR_LAYOUT_FILE file not found"
+fi
+
+# Execute the file listed in this file
+$(< "$MONITOR_LAYOUT_FILE")
+
 if [ "$?" -eq 0 ]; then
 	notify-send "Monitor Layout" "Recreation succeded"
 else
